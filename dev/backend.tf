769a5fa7504dea5b137850fc1d989fac03f3a62d
Krishna <thisisyogesh1984@gmail.com>
2020-07-15 14:23:07 +0000

    Updated backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,5 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "tfbackendstorageone"
-    container_name       = "tfbackendcontainerone"
-    access_key           = "+O7Ceet1tS9KTTQ8CK7kM7ZlgwILl9JEU2onhvOinrjkcTTeK9E0QakpfkSK0hFeZQ1flSOUdxvT/mYYcZ1GBw=="
-    key                  = "tf.tfstate"
-    resource_group_name  = "tfdependcies"
-  }
-}
+storage_account_name = "tfbackendstorageone"
+container_name       = "tfbackendcontainerone"
+access_key           = "+O7Ceet1tS9KTTQ8CK7kM7ZlgwILl9JEU2onhvOinrjkcTTeK9E0QakpfkSK0hFeZQ1flSOUdxvT/mYYcZ1GBw=="
+key                  = "tf.tfstate"
+resource_group_name  = "tfdependcies"
