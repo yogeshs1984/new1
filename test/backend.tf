8151cf86070e4b0c57676605199a40bfe5c0f515
Krishna <thisisyogesh1984@gmail.com>
2020-07-15 14:28:36 +0000

    Added backend.tf

---
+++ test/backend.tf
@@ -0,0 +1,5 @@
+storage_account_name = "tfbackendstorageone"
+container_name       = "tfbackendcontainerone"
+access_key           = "+O7Ceet1tS9KTTQ8CK7kM7ZlgwILl9JEU2onhvOinrjkcTTeK9E0QakpfkSK0hFeZQ1flSOUdxvT/mYYcZ1GBw=="
+key                  = "att/test-terraform.tfstate"
+resource_group_name  = "tfdependcies"
