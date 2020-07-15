1297621d3dc0312d85aa18d21ffd8e86dd1f2f33
Krishna <thisisyogesh1984@gmail.com>
2020-07-15 14:09:28 +0000

    Added backend.tf

---
+++ dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tfbackendstorageone"
+    container_name       = "tfbackendcontainerone"
+    access_key           = "+O7Ceet1tS9KTTQ8CK7kM7ZlgwILl9JEU2onhvOinrjkcTTeK9E0QakpfkSK0hFeZQ1flSOUdxvT/mYYcZ1GBw=="
+    key                  = "tf.tfstate"
+    resource_group_name  = "tfdependcies"
+  }
+}
