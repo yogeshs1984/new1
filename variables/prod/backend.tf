0264626a6d3930baacc2491d6718580304348e51
Krishna <thisisyogesh1984@gmail.com>
2020-07-16 10:30:38 +0000

    Updated backend.tf

--- variables/prod/backend.tf
+++ variables/prod/backend.tf
@@ -1,5 +1,5 @@
 storage_account_name = "tfbackendstorageone"
 container_name       = "tfbackendcontainerone"
 access_key           = "+O7Ceet1tS9KTTQ8CK7kM7ZlgwILl9JEU2onhvOinrjkcTTeK9E0QakpfkSK0hFeZQ1flSOUdxvT/mYYcZ1GBw=="
-key                  = "att/prod-terraform.tfstate"
+key                  = "projects/ddet/dev-terraform.tfstate"
 resource_group_name  = "tfdependcies"
