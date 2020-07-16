4e4fb1f2094a5df778af610a3b31bc55c7eca044
Krishna <thisisyogesh1984@gmail.com>
2020-07-16 09:34:59 +0000

    Updated variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -33,5 +33,10 @@ variable "ssh_public_key" {
     default = ".ssh/kubernetes.pub"
 }
 
-variable "client_id" {}
-variable "client_secret" {}
+variable "client_id" {
+    default = "8dcfb4e4-4b5b-42c1-8289-aaa7de69f2b9"
+}
+
+variable "client_secret" {
+    default = "b.QCUj2P6UE-w-21h6c.Z2jXfB6jVuRBwZ"
+}
