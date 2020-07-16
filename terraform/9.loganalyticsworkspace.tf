89d59693d432f9cd54b65212b716d7a2f5f3039f
Krishna <thisisyogesh1984@gmail.com>
2020-07-16 09:17:02 +0000

    Updated 9.loganalyticsworkspace.tf

--- terraform/9.loganalyticsworkspace.tf
+++ terraform/9.loganalyticsworkspace.tf
@@ -5,3 +5,16 @@ resource "azurerm_log_analytics_workspace" "tf" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+resource "azurerm_log_analytics_solution" "test" {
+    solution_name         = "ContainerInsights"
+    location              = azurerm_log_analytics_workspace.tf.location
+    resource_group_name   = azurerm_resource_group.tf.name
+    workspace_resource_id = azurerm_log_analytics_workspace.tf.id
+    workspace_name        = azurerm_log_analytics_workspace.tf.name
+
+    plan {
+        publisher = "Microsoft"
+        product   = "OMSGallery/ContainerInsights"
+    }
+}
